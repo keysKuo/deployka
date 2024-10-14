e39e6188466be1ef03d0802f4328b9efbf4e79e3
nKeys Kuo <nkeyskuo124@gmail.com>
2024-10-14 21:52:04 +0700

    add nginx conf and run code

--- deploy/src/index.ts
+++ deploy/src/index.ts
@@ -10,13 +10,14 @@ async function main () {
         const response = await subscriber.brPop(commandOptions({ isolated: true }), 'build-queue', 0);
 
         const storedId = response?.element;
+        if (!storedId) return;
         console.log(storedId);
 
         // Download source code by its id from AWS S3
         await downloadFromS3(R2_BUCKET, `sources/${storedId}`);
 
         // Build the project in sight
-        await buildProject(`${OUTPUT_DIR}/${storedId}`);
+        await buildProject(storedId, `${OUTPUT_DIR}/${storedId}`);
 
         // Upload the built files back to AWS S3
         await uploadFolderToS3(R2_BUCKET, `builds`,`${OUTPUT_DIR}/${storedId}/.next`);
--- deploy/src/middlewares/project.build.ts
+++ deploy/src/middlewares/project.build.ts
@@ -1,5 +1,6 @@
 import { exec } from 'child_process';
-import path from 'path';
+import fs from 'fs';
+import { DOMAIN } from '../constants';
 
 const runCommand = (command: string, workingDir: string) => {
     return new Promise((resolve, reject) => {
@@ -24,16 +25,63 @@ const runCommand = (command: string, workingDir: string) => {
     })
 };
 
-export const buildProject = async (folderPath: string) => {
+export const buildProject = async (storedId: string, folderPath: string) => {
     try {
         console.log('📦 Installing dependencies...');
         await runCommand('npm install', folderPath);
 
         console.log('🚀 Building the project...');
         await runCommand('npm run build', folderPath);
-        
+
+        console.log('⚙️ Setting nginx config...');
+        const randomPort = Math.floor(Math.random() * 10000);
+        const nginxConfPath = `${folderPath}/deployka-${storedId}.conf`;
+        createNginxConf(`deployka-${storedId}.${DOMAIN}`, randomPort, nginxConfPath);
+        // await runCommand(`docker cp ${nginxConfPath} nginx:/etc/nginx/conf.d/deployka-${storedId}.conf`, folderPath);
+
+        console.log('🌐 Running the project...');
+        await runCommand(`pm2 start next --name 'deployka-${storedId}' start -p ${randomPort}`, folderPath);
+
         console.log('✔️ Project built successfully.');
     } catch (error) {
         console.error('Error during the build process:', error);
     }
 };
+
+export const createNginxConf = (domain: string, port: number, filePath: string) => {
+    const nginxContent =
+    `
+    server {
+        listen 80;
+        server_name ${domain};
+
+        location / {
+            proxy_pass http://localhost:${port};
+            proxy_http_version 1.1;
+            proxy_set_header Upgrade $http_upgrade;
+            proxy_set_header Connection 'upgrade';
+            proxy_set_header Host $host;
+            proxy_cache_bypass $http_upgrade;
+        }
+
+        location /_next/static/ {
+            alias /var/www/monbeauty.org/.next/static/;
+            expires 1y;
+            access_log off;
+        }
+
+        gzip on;
+        gzip_types text/plain text/css application/json application/javascript text/xml application/xml application/xml+rss text/javascript;
+        gzip_proxied any;
+        gzip_min_length 256;
+    }
+    `
+
+    fs.writeFile(filePath, nginxContent, (err) => {
+        if (err) {
+            console.error('Có lỗi khi tạo file nginx.conf:', err);
+            return;
+        }
+        console.log(`File ${filePath} đã được tạo thành công.`);
+    });
+}
