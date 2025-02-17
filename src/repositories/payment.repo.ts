5094c4e7db864e365434edcabab0d1ce9255756d
nKeys Kuo <nkeyskuo124@gmail.com>
2025-02-17 12:51:07 +0700

    rm: uneccessary files

--- src/repositories/payment.repo.ts
+++
@@ -1,7 +0,0 @@
-import { StripeItem } from "../entities/payment.entity";
-
-export interface PaymentRepository {
-	createSession(items: StripeItem[]): Promise<any>;
-	retrieve(): Promise<any>;
-}
-
--- src/repositories/upload.repo.ts
+++
@@ -1,9 +0,0 @@
-export type UploadResponse = {
-    success: boolean,
-    uploadDir?: string,
-    error?: string
-}
-
-export interface UploadRepository {
-    upload(repoUrl: string): Promise<UploadResponse>;
-}
